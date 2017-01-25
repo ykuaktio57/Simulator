c8b677b515cd1190630e2d1db4ee3c4d21a6d3a2
yuki kato <kktaiyou57@yahoo.co.jp>
2017-01-25 16:20:21 +0900

    add main script

---
+++ Human.cs
@@ -0,0 +1,287 @@
+using UnityEngine;
+using System.Collections;
+using System.IO;
+using System;
+
+public class Human : StartButton {
+
+	public GameObject BoxUnityChan;
+	public GameObject BoxUnityChan_Lying;
+	public GameObject BoxUnityChan_Sitting;
+	string csvname;
+
+	public bool alchemize_human;
+	public bool change_pose;
+	public bool change_position;
+
+
+	int pos_set(float xa,float xi,float za,float zi,GameObject hito){
+		// 位置ラベル付け用変数
+		float pointx = (xa - xi) / 3.0f;
+		float pointz = (za - zi) / 3.0f;
+		//Debug.Log ("pointx" + pointx);
+		//Debug.Log ("pointz" + pointz);
+		GameObject kabex = GameObject.Find ("Kabe04");
+		GameObject kabez = GameObject.Find ("Kabe02");
+		float wallx = kabex.transform.position.x;
+		float wallz = kabez.transform.position.z;
+		//Debug.Log ("wallx" + wallx);
+		//Debug.Log ("wallz" + wallz);
+		//float disx = Vector2.Distance (hito.transform.position.x, wallx);
+		//float disz = Vector2.Distance (hito.transform.position.z, wallz);
+		float disx = hito.transform.position.x - wallx;
+		float disz = hito.transform.position.z - wallz;
+		//Debug.Log ("人の位置" + hito.transform.position);
+		//Debug.Log ("disx" + disx);
+		//Debug.Log ("disz" + disz);
+
+		int pos = -1;
+
+		// 出現位置によってラベル付け
+		if (disx < pointx && disz < pointz) {
+			pos = 0;
+		} else if (pointx < disx & disx < pointx * 2.0f && disz < pointz) {
+			pos = 1;
+		} else if (pointx * 2.0f < disx & disx < pointx * 3.0f && disz < pointz) {
+			pos = 2;
+		} else if (disx < pointx && pointz < disz & disz < pointz * 2.0f) {
+			pos = 3;
+		} else if (pointx < disx & disx < pointx * 2.0f && pointz < disz & disz < pointz * 2.0f) {
+			pos = 4;
+		} else if (pointx * 2.0f < disx & disx < pointx * 3.0f && pointz < disz & disz < pointz * 2.0f) {
+			pos = 5;
+		} else if (disx < pointx && pointz * 2.0f < disz & disz < pointz * 3.0f) {
+			pos = 6;
+		} else if (pointx < disx & disx < pointx * 2.0f && pointz * 2.0f < disz & disz < pointz * 3.0f) {
+			pos = 7;
+		} else if (pointx * 2.0f < disx & disx < pointx * 3.0f && pointz * 2.0f < disz & disz < pointz * 3.0f) {
+			pos = 8;
+		} else {
+			Debug.Log ("nohit");
+		}
+		return pos;
+	}
+
+/*
+	void Get_pos(GameObject hito){
+		Vector3 human_pos = hito.transform.position;
+		return human_pos;
+	}
+*/
+
+	void Start(){
+		GameObject sensor = GameObject.Find ("sensor");
+		csvname=sensor.GetComponent <ray_information>().csvname;
+		Application.targetFrameRate = 120;
+	}
+
+	//ボタンを押したら部屋サイズを変えるスクリプトを呼び出す
+	protected override void OnClick(string objectName) {
+		if ("Button06".Equals (objectName)) {
+			this.Button06Click ();
+		} else if ("Button08".Equals (objectName)) {
+			this.Button08Click ();
+		} else if ("Button10".Equals (objectName)) {
+			this.Button10Click ();
+		} else {
+			throw new System.Exception ("Not implemented!!");
+		}
+	}
+
+	//押したボタンによって部屋のスケールを変える
+	private void Button06Click() {
+		GameObject.Find ("House01").transform.localScale = new Vector3 (0.28125f, 0.7f, 0.375f);
+		//StartCoroutine ("HumanAlchemize");
+	}
+
+	private void Button08Click (){
+		GameObject.Find ("House01").transform.localScale = new Vector3 (0.375f, 0.7f, 0.375f);
+		//StartCoroutine ("HumanAlchemize");
+	}
+
+	private void Button10Click (){
+		GameObject.Find ("House01").transform.localScale = new Vector3 (0.375f, 0.7f, 0.46875f);
+		//StartCoroutine ("HumanAlchemize");
+	}
+
+	int num;
+	int pos;
+	Vector3 human_pos;
+
+	//人体錬成スクリプト
+	public void CreateHuman(){
+		if (human != null) {
+			Destroy (human);
+		}
+		if (alchemize_human == true) {  //trueで人体錬成を行う
+
+			//numはポーズナンバー
+			//0が起立、1が着座、2が横たわる
+			num = UnityEngine.Random.Range (0, 3);
+
+			//人体が部屋の外に錬成されないよう、部屋のサイズを取得
+			Vector3 size = GameObject.Find ("House01").transform.lossyScale;
+			float xs = size.x;
+			//float ys = size.y;   //部屋の高さをいじりたい場合は使う
+			float zs = size.z;
+
+			// 人間を錬成する範囲を指定
+			// -5f,4.5fは壁の位置座標の最小と最大、それにスケールを掛けることで位置が算出される
+			float xmin = -5f * xs;
+			float xmax = 4.5f * xs;
+			float zmin = -5f * zs;
+			float zmax = 4.5f * zs;
+
+			// 人間を錬成する対位/位置/角度
+			// 位置を変えたいときはVector3のxとzの、角度を変えたい時はquaternionのyの値をいじる
+			if (num == 0) {
+				if (change_position) {
+					human = (GameObject)GameObject.Instantiate (BoxUnityChan, new Vector3 (UnityEngine.Random.Range (xmin, xmax), 0, UnityEngine.Random.Range (zmin, zmax)), Quaternion.Euler (0, UnityEngine.Random.Range (0.0f, 359.0f), 0));
+				} else {
+					human = (GameObject)GameObject.Instantiate (BoxUnityChan, new Vector3 (0, 0, 0), Quaternion.Euler (0, UnityEngine.Random.Range (0.0f, 359.0f), 0)); 
+				}
+			} else if (num == 1) {
+				if (change_position) {
+					human = (GameObject)GameObject.Instantiate (BoxUnityChan_Lying, new Vector3 (UnityEngine.Random.Range (xmin, xmax), 0, UnityEngine.Random.Range (zmin, zmax)), Quaternion.Euler (270, UnityEngine.Random.Range (0.0f, 359.0f), 0));
+				} else {
+					human = (GameObject)GameObject.Instantiate (BoxUnityChan_Lying, new Vector3 (0, 0, 0), Quaternion.Euler (270, UnityEngine.Random.Range (0.0f, 359.0f), 0));
+				}
+			} else if (num == 2) {
+				if (change_position) {
+					human = (GameObject)GameObject.Instantiate (BoxUnityChan_Sitting, new Vector3 (UnityEngine.Random.Range (xmin, xmax), 0, UnityEngine.Random.Range (zmin, zmax)), Quaternion.Euler (0, UnityEngine.Random.Range (0.0f, 359.0f), 0));
+				} else {
+					human = (GameObject)GameObject.Instantiate (BoxUnityChan_Sitting, new Vector3 (0, 0, 0), Quaternion.Euler (0, UnityEngine.Random.Range (0.0f, 359.0f), 0));
+				}
+			}
+			pos = pos_set (xmax, xmin, zmax, zmin, human); //部屋を9分割したポジション
+			//human_pos = Get_pos (human); //座標を取得
+		}
+	}
+	/*
+	public void GetHumanCoordinate(){
+		Vector3 coordinate = human_pos;
+		return coordinate;
+	}
+	*/
+	GameObject human;
+	int labelcount=0; //デバッグログ用
+
+	//ラベルづけ
+	public void GetHumanLabel(){
+		GameObject alchemize = GameObject.Find ("Alchemize");
+		if (alchemize.GetComponent<SimulationManager> ().isstart) {
+			//Debug.Log ("update");
+			/* if (Input.GetKey (KeyCode.Alpha1)) { 
+			StopCoroutine ("HumanAlchemize");
+		} */
+			if (num == 0) {
+				labelSave ("pose", num);
+				labelSave ("position", pos);
+			} else if (num == 1){
+				labelSave ("pose", num);
+				labelSave ("position", pos);
+			} else if (num == 2) {
+				labelSave ("pose", num);
+				labelSave ("position", pos);
+			}
+			labelcount++;  //デバッグログ用
+			Debug.Log ("labelcount"+labelcount);  //デバッグログ用
+		}
+	}
+
+	/*
+	// 人体錬成コルーチン (HumanAlchemize = 人体錬成)
+	IEnumerator HumanAlchemize() {
+		while (true) {
+			
+			Vector3 size = GameObject.Find("House01").transform.lossyScale;
+			float xs = size.x;
+			//float ys = size.y;   //まあ別にいらない
+			float zs = size.z;
+
+			// 人間を錬成する範囲
+			float xmin = -5f * xs;
+			float xmax = 4.5f * xs;
+			float zmin = -5f * zs;
+			float zmax = 4.5f * zs;
+
+			int num = UnityEngine.Random.Range(0, 3);
+			if (num == 0) {
+				GameObject human = (GameObject)GameObject.Instantiate (boxunitychan, new Vector3 (UnityEngine.Random.Range (xmin, xmax), 0, UnityEngine.Random.Range (zmin, zmax)), Quaternion.Euler (0, UnityEngine.Random.Range (0.0f, 359.0f), 0));
+				int pos = pos_set (xmax,xmin,zmax,zmin,human);
+				labelSave ("pose", num);
+				labelSave ("position", pos);
+				//yield return new WaitForSeconds (0.03f);
+				yield return new WaitForEndOfFrame();
+				Destroy (human);
+			} else if (num == 1) {
+				GameObject human = (GameObject)GameObject.Instantiate (BoxUnityChan_Lying, new Vector3 (UnityEngine.Random.Range (xmin, xmax), 0, UnityEngine.Random.Range (zmin, zmax)), Quaternion.Euler (270, UnityEngine.Random.Range (0.0f, 359.0f), 0));
+				int pos = pos_set (xmax,xmin,zmax,zmin,human);
+				labelSave ("pose", num);
+				labelSave ("position", pos);
+				//yield return new WaitForSeconds (0.03f);
+				yield return new WaitForEndOfFrame();
+				Destroy (human);
+			} else if (num == 2) {
+				GameObject human = (GameObject)GameObject.Instantiate (BoxUnityChan_Sitting, new Vector3 (UnityEngine.Random.Range (xmin, xmax), 0, UnityEngine.Random.Range (zmin, zmax)), Quaternion.Euler (0, UnityEngine.Random.Range (0.0f, 359.0f), 0));
+				int pos = pos_set (xmax,xmin,zmax,zmin,human);
+				labelSave("pose",num);
+				labelSave ("position", pos);
+				//yield return new WaitForSeconds (0.03f);
+				yield return new WaitForEndOfFrame();
+				Destroy (human);
+			}
+			Debug.Log ("a");
+		}
+	}
+	:*/
+ 
+	void labelSave(string name,float label) {
+		//Debug.Log(height);
+		StreamWriter sw;
+		FileInfo fi;
+		fi = new FileInfo(csvname+"_"+name + "_label.csv");
+		sw = fi.AppendText();
+		//string line = string.Format("{0},{1}",
+		//11, 13);
+		//クラス分け用(distance)
+		int l=(int)label;
+
+		if (name == "pose") {
+			l = (int)(label);
+		}
+
+		if (name == "position"){
+			l = (int)(label);
+		}
+
+		/*
+		if (name == "x") {
+			l = (int)(label * 2);
+		} else if (name == "y") {
+			l = (int)(label);
+		} else if (name == "z") {
+			l = (int)(label*2)+9;
+		}
+
+		if (name == "distance") {
+			l = Mathf.RoundToInt(label * 2) - 2;
+			if (l < 0) l = 0;
+		} else if (name == "height") {
+			l = Mathf.RoundToInt(label * 2) - 2;
+			//l = Mathf.RoundToInt(label ) ;
+			if (l < 0) l = 0;
+		} else if (name == "angle") {
+			l = Mathf.RoundToInt(label);
+			l = l / 15;
+			if (l < 0) l = 0;
+			else if (l > 6) l = 6;
+		}
+		*/
+		sw.Write("{0},", l);
+		sw.Write(sw.NewLine);
+		//sw.Write(line);
+		sw.Flush();
+		sw.Close();
+	}
+}
---
+++ ray_information.cs
@@ -0,0 +1,236 @@
+using UnityEngine;
+using System.Collections;
+using System.IO;
+using System;
+
+public class ray_information : MonoBehaviour {
+    public float x;
+    public float y;
+    public float z;
+
+    public bool is_ramdom;
+    public bool is_change_angle;
+
+    public string csvname;
+    //need infomation
+    float distance;
+    float height;
+    float angle;
+    float vec;
+    float person_with_sensor_angle_acquisition;
+    Vector3 sensor_angle;
+	Vector3 sensor_pos;
+    bool is_hitting;
+    int log_num;
+	int rtem;
+    struct ray_struct
+    {
+        public Ray ray;
+        public int num;
+        public bool hit;
+    }
+    ray_struct[] ray = new ray_struct[64];
+    
+    void Start()
+    {
+        Application.targetFrameRate = 120;
+        ray_set();
+        is_hitting = false;
+        log_num = 0;
+    }
+
+
+
+    void ray_set() {
+        for (int i = 0; i <= 63; i++)
+        {
+            Vector3 direction = Vector3.down;
+            
+            direction = Quaternion.Euler(30f + (-(60f / 7f) * (i % 8)), 0f, 30f + (-(60f / 7f) * ((i) / 8))) * direction;
+            direction = Quaternion.Euler(this.gameObject.transform.rotation.eulerAngles) * direction;
+            //-60 -
+            //if (i%8==0)
+            //if(i>=0&&i<8)
+                ray[i].ray = new Ray(this.gameObject.transform.position, direction);
+            
+        }
+    }
+	//センサ位置ランダム機能
+    void Random_sensor()
+    {
+        Vector3 pos= this.gameObject.transform.position;
+        Vector3 ang=this.gameObject.transform.eulerAngles;
+
+        //pos.x = Mathf.RoundToInt(UnityEngine.Random.Range(1.0f + 0.00001f, 4.5f - 0.00001f) *2)/2.0f;
+        pos.x = -1.85f +Mathf.RoundToInt(UnityEngine.Random.Range(-x + 0.00001f, x - 0.00001f) * 2) / 2.0f;
+        pos.y = 1.4f +Mathf.RoundToInt(UnityEngine.Random.Range(0.0f+0.00001f, y - 0.00001f) *2)/2.0f;
+        //pos.y = UnityEngine.Random.Range(0.5f , 3.5f );
+        pos.z = 0+Mathf.RoundToInt(UnityEngine.Random.Range(-z + 0.00001f, z- 0.00001f) *2)/2.0f;
+        //pos.z = UnityEngine.Random.Range(-4.0f , 4.0f );
+        
+        //pos.z = Mathf.RoundToInt(UnityEngine.Random.Range(0, 2));
+        if (pos.z == 0)
+            pos.z = -1;
+        ang.x = Mathf.RoundToInt(UnityEngine.Random.Range(0.0f, 90.0f) / 15.0f) * 15;
+        //ang.x = Mathf.RoundToInt(UnityEngine.Random.Range(0.0f-14.99999999f, 90.0f + 14.99999999f) / 15.0f) * 15;
+        this.gameObject.transform.position = pos;
+        if(is_change_angle)this.gameObject.transform.eulerAngles=ang;
+    }
+
+	int datacount=0;
+	void Update () {
+		GameObject alchemize = GameObject.Find ("Alchemize");
+		Vector3 pos= this.gameObject.transform.position; //センサの位置
+		if (alchemize.GetComponent<SimulationManager> ().isstart) {
+			Human human = GameObject.Find ("Alchemize").GetComponent<Human> ();
+			human.CreateHuman ();
+			//human.GetHumanCoordinate ();
+			ray_set ();
+			RaycastHit hit;
+			is_hitting = false;  
+			float min_distance = 6;
+			GameObject House01 = GameObject.Find ("House01");  
+			rtem = House01.GetComponent <room_temperature>().room_tem;  
+			if (is_ramdom)
+				Random_sensor ();  
+			for (int i = 0; i <= 63; i++) {
+				if (Physics.Raycast (ray [i].ray, out hit, 5.0f)) {
+					if (hit.transform.gameObject.tag == "human") {
+						is_hitting = true;
+						ray [i].num = hit_any (ray [i], hit);
+						if (min_distance > hit.distance)
+							min_distance = hit.distance;
+						// ヒットしたオブジェクトのカラーを変更
+						//hit.transform.gameObject.GetComponent<Renderer>().material.color = Color.red;
+						//Debug.Log(hit.distance);
+						//Debug.Log(person_with_sensor_angle_acquisition(this.gameObject.transform.rotation.eulerAngles, ray[i].ray.direction));
+						//Debug.Log(Vector3.Angle(this.gameObject.transform.position, ray[i].ray.direction));
+						if (this.gameObject.transform.position.z < hit.transform.gameObject.transform.position.z)
+							vec = 1;
+						else
+							vec = 0;
+					}
+				} else
+					//ray [i].num = 12 * 4 + UnityEngine.Random.Range (0, 8); //室温
+					ray [i].num = rtem * 4 + UnityEngine.Random.Range (0, 8);
+				//ray[i].num = 12 * 4;
+				//if(i/8==0)
+				Debug.DrawRay (ray [i].ray.origin, ray [i].ray.direction * 5.0f, Color.red);
+            
+			}
+			distance = min_distance;
+			height = this.gameObject.transform.position.y;
+			angle = this.gameObject.transform.rotation.eulerAngles.x;
+			if (is_hitting) {
+				logSave ();
+				Debug.Log (log_num);
+				datacount++;
+				Debug.Log ("datacount" + datacount);
+				//GameObject.Find ("Alchemize").GetComponent<Human> ().GetHumanLabel ();
+				human.GetHumanLabel();
+			}
+		}
+    }
+	int hit_any(ray_struct ray,RaycastHit hit)  
+	{
+        ray.num = hit.transform.gameObject.GetComponent<temperature_information>().temperature + UnityEngine.Random.Range(0, 12);
+			return ray.num;
+    }
+    void angle_infomation(Vector3 hit_pos)
+    {
+        person_with_sensor_angle_acquisition= Vector3.Angle(this.gameObject.transform.position, hit_pos);
+        sensor_angle = this.gameObject.transform.rotation.eulerAngles;
+    }
+    
+    public void logSave()
+    {
+        StreamWriter sw;
+        FileInfo fi;
+        //fi = new FileInfo("koteiangle_2m_test6.csv");
+        fi = new FileInfo(csvname+".csv");
+        sw = fi.AppendText();
+        log_num += 1;
+        //string line = string.Format("{0},{1}",
+        //11, 13);
+
+        //正規化
+        float data;
+        for (int i = 0; i <= 63; i++)
+        {
+            //ray[i].num -= (int)(2 * (distance - 1));
+            data=(float)ray[i].num / 100f;
+            data = (float)ray[i].num ;
+            sw.Write("{0},", data);
+        }
+        sw.Write(sw.NewLine);
+
+        //sw.Write(line);
+        sw.Flush();
+        sw.Close();
+
+        //--------------------------------------------------
+        //ここから先がラベルについて labelSave("ファイル名", 変数);
+        //--------------------------------------------------
+        //labelSave("leftright", vec);
+        //labelSave("distance", distance);
+        //labelSave("angle", angle);
+        //labelSave("height", height);
+        
+          
+    //  labelSave("x", this.gameObject.transform.position.x);
+        //labelSave("y", height);
+        //labelSave("z", this.gameObject.transform.position.z);
+    }
+    
+	void labelSave(string name,float label)
+    {
+        //Debug.Log(height);
+        StreamWriter sw;
+        FileInfo fi;
+        fi = new FileInfo("random_"+name + csvname+".csv");
+        sw = fi.AppendText();
+        //string line = string.Format("{0},{1}",
+        //11, 13);
+        //クラス分け用(distance)
+        int l=(int)label;
+        if (name == "x")
+        {
+            l = (int)(label * 2);
+        }
+        else if (name == "y")
+        {
+            l = (int)(label);
+        }
+        else if (name == "z")
+        {
+            l = (int)(label*2)+9;
+        }
+        if (name == "distance")
+        {
+            
+
+            l = Mathf.RoundToInt(label * 2) - 2;
+            
+            if (l < 0) l = 0;
+        }
+        else if (name == "height")
+        {
+            l = Mathf.RoundToInt(label * 2) - 2;
+            //l = Mathf.RoundToInt(label ) ;
+            if (l < 0) l = 0;
+        }
+        else if (name == "angle")
+        {
+            l = Mathf.RoundToInt(label);
+            l = l / 15;
+            if (l < 0) l = 0;
+            else if (l > 6) l = 6;
+        }
+        sw.Write("{0},", l);
+        sw.Write(sw.NewLine);
+        //sw.Write(line);
+        sw.Flush();
+        sw.Close();
+    }
+
+}
